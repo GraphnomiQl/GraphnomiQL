9839204bcbf502971a60047f7fbbb2d26cd85d53
vwang4536 <vwang4536@gmail.com>
2018-12-20 11:39:39 -0800

    completed essential actions, action types, reducers, and initial states; now need to work on dispatch, components, etc

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,4 +1,18 @@
-// action type for changing schemas under introspectionActions.js
+// action type for changing schemas under introspectionActions.js (WORKS ALONG WITH SVG RENDERING COMPLETED)
 export const CHANGE_SCHEMA = 'CHANGE_SCHEMA';
 // action type that focuses in on one schema table
 export const SELECT_NODE = 'SELECT_NODE';
+// action type that focuses on an edge relationship between a field and a node table
+export const SELECT_EDGE = 'SELECT_EDGE';
+// action type that allows zooming in on an node or edge
+export const FOCUS_ELEMENT = 'FOCUS_ELEMENT';
+// action type that completes focusing on node or edge
+export const FOCUS_ELEMENT_DONE = 'FOCUS_ELEMENT_DONE';
+// action type that renders svg string and complete the schemaView (WORKS ALONG WITH CHANGE_SCHEMA)
+export const SVG_RENDERING_COMPLETED = 'SVG_RENDERING_COMPLETED';
+// action type that clears selection of a node, field, or edge
+export const CLEAR_SELECTION = 'CLEAR_SELECTION';
+// action type for reporting any errors that come up
+export const REPORT_ERROR = 'REPORT_ERROR';
+// action type that clears up error (unsure what it still does)
+export const CLEAR_ERROR = 'CLEAR_ERROR';
