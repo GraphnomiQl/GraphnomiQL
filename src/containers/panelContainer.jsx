23107ebdbd1e994628aa388c56c124742769ce8c
Donte' Nall <code4zelda@gmail.com>
2018-12-26 13:53:17 -0800

    fix table position

--- src/containers/panelContainer.jsx
+++ src/containers/panelContainer.jsx
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 
 // entire side panel component
-  // top - schema selection
-  // middle - type info
-  // botton - editing area
+// top - schema selection
+// middle - type info
+// botton - editing area
 class Panel extends Component {
   constructor(props) {
     super(props);
@@ -14,8 +14,13 @@ class Panel extends Component {
   render() {
     return (
       <div class="panel">
-        <label>Select Your Schema Here!</label>
-        <Button id="ChangeSchema" onClick={this.props.handleOpen}>Change Schema</Button>
+        <div className='center'>
+          <label>Select Your Schema Here!</label>
+          <br />
+          <br />
+
+          <Button id="ChangeSchema" onClick={this.props.handleOpen}>Change Schema</Button>
+        </div>
       </div>
     )
   }
