fe1a329394d435cf180db5f287a15cbc991cdb7c
Donte' Nall <code4zelda@gmail.com>
2018-12-26 17:44:31 -0800

    fixed header in the tables

--- src/containers/modalContainer.jsx
+++ src/containers/modalContainer.jsx
@@ -101,7 +101,7 @@ class ModalContainer extends React.Component {
     return (
       <div>
         {/* <Button id="ChangeSchema" onClick={this.handleOpen}>Change Schema</Button> */}
-        <Modal
+        <Modal className="model-container"
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
           open={open}
